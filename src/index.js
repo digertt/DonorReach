be4963e1388ee78292258cd1784b9cb90cd7bf05
Caroline <caroline.kays@live.com>
2023-11-03 13:41:38 -0700

    Added progress bars for donation tracking
    + linked style and script pages
    + info before subscription pages
    + title

--- src/index.js
+++ src/index.js
@@ -8,3 +8,20 @@ registerGlobalComponents()
 
 // mount all the global components found in this page
 mount('[data-riot-component]')
+
+// Function to create progress bar "fractions"
+function updateCustomFractions() {
+    const donationItems = document.querySelectorAll('.donation-item');
+  
+    donationItems.forEach(item => {
+      const progressBar = item.querySelector('.progress-fill');
+      const customFractionText = item.querySelector('.custom-fraction-text');
+  
+      // get custom fraction text from element's content
+      const customFraction = customFractionText.textContent;
+      customFractionText.textContent = customFraction;
+    });
+  }
+  
+  //update when the page loads
+  window.addEventListener('load', updateCustomFractions);
